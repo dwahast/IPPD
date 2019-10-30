4bfa70e52132de2a038a2d5dd88171aa7737783a
Douglas Wahast da Costa <douglaswahastdacosta@pop-os.localdomain>
2019-10-30 17:03:18 -0300

    iniciação da string somente no rank 0

--- send_recv_message_mpi.c
+++ send_recv_message_mpi.c
@@ -11,23 +11,20 @@ void main(){
 
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
-	MPI_Comm_size(MPI_COMM_WORLD, &world_size);
-
-	char s[4] = "Ola!";
-	int len_msg = strlen(s);
-	printf("tamanho da msg: %d\n",len_msg);		
+	MPI_Comm_size(MPI_COMM_WORLD, &world_size);		
 		
 	if(my_rank == 0){
+		char s[4] = "Ola!";
 		printf("World_size:%d\n",world_size);
 		printf("\n ------------------------------- \n");
 		
 		printf("Eu (%d) Enviei: '%s'\n",my_rank, s);
 		
-		MPI_Send(&s, len_msg, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
+		MPI_Send(&s, 4, MPI_BYTE, 1, 0, MPI_COMM_WORLD);
 	
 	}else{
-		
-		MPI_Recv(&s, len_msg, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+		char s[4];		
+		MPI_Recv(&s, 4, MPI_BYTE, 0, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 		printf("Eu (%d) recebi: %s\n",my_rank, s);
 	}
 	MPI_Finalize();	
